05d3f09197160f489d64f9d0fb997df8225bc470
Narelsiak <michalekxddlol@gmail.com>
2025-04-29 23:09:19 +0200

    post reply model

---
+++ app/models/post_reply.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Post from './post.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class PostReply extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare postId: number // Post, który jest cytowany
+
+  @column()
+  declare replyId: number // Post, który cytuje
+
+  @belongsTo(() => Post, {
+    foreignKey: 'postId',
+  })
+  declare quotedPost: BelongsTo<typeof Post>
+
+  @belongsTo(() => Post, {
+    foreignKey: 'replyId',
+  })
+  declare quotingPost: BelongsTo<typeof Post>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
