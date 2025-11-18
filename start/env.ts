b9aa3187a96920438d9f41293ea7c6dc4da227c1
Tomasz Slapinski <tom.slapinski@gmail.com>
2025-11-18 17:17:04 +0100

    another try

--- start/env.ts
+++ start/env.ts
@@ -12,49 +12,49 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
-  REDIS_HOST: Env.schema.string({ format: 'host' }),
-  REDIS_PORT: Env.schema.number(),
-  REDIS_PASSWORD: Env.schema.string.optional(),
-  /*
-  |----------------------------------------------------------
-  | Variables for CORS
-  |----------------------------------------------------------
-  */
-  CORS_ALLOWED_ORIGINS: Env.schema.string(),
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring the limiter package
-  |----------------------------------------------------------
-  */
-  LIMITER_STORE: Env.schema.enum(['database', 'memory'] as const),
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring session package
-  |----------------------------------------------------------
-  */
-  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
-  /*
-  |----------------------------------------------------------
-  | Variables for testing
-  |----------------------------------------------------------
-  */
-  USER_LOGIN: Env.schema.string(),
-  USER_PASSWORD: Env.schema.string(),
-  BANNED_USER_LOGIN: Env.schema.string(),
-  BANNED_USER_PASSWORD: Env.schema.string(),
+  // NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  // PORT: Env.schema.number(),
+  // APP_KEY: Env.schema.string(),
+  // HOST: Env.schema.string({ format: 'host' }),
+  // LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+  // /*
+  // |----------------------------------------------------------
+  // | Variables for configuring database connection
+  // |----------------------------------------------------------
+  // */
+  // DB_HOST: Env.schema.string({ format: 'host' }),
+  // DB_PORT: Env.schema.number(),
+  // DB_USER: Env.schema.string(),
+  // DB_PASSWORD: Env.schema.string.optional(),
+  // DB_DATABASE: Env.schema.string(),
+  // REDIS_HOST: Env.schema.string({ format: 'host' }),
+  // REDIS_PORT: Env.schema.number(),
+  // REDIS_PASSWORD: Env.schema.string.optional(),
+  // /*
+  // |----------------------------------------------------------
+  // | Variables for CORS
+  // |----------------------------------------------------------
+  // */
+  // CORS_ALLOWED_ORIGINS: Env.schema.string(),
+  // /*
+  // |----------------------------------------------------------
+  // | Variables for configuring the limiter package
+  // |----------------------------------------------------------
+  // */
+  // LIMITER_STORE: Env.schema.enum(['database', 'memory'] as const),
+  // /*
+  // |----------------------------------------------------------
+  // | Variables for configuring session package
+  // |----------------------------------------------------------
+  // */
+  // SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
+  // /*
+  // |----------------------------------------------------------
+  // | Variables for testing
+  // |----------------------------------------------------------
+  // */
+  // USER_LOGIN: Env.schema.string(),
+  // USER_PASSWORD: Env.schema.string(),
+  // BANNED_USER_LOGIN: Env.schema.string(),
+  // BANNED_USER_PASSWORD: Env.schema.string(),
 })
